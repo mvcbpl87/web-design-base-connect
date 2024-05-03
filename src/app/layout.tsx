464099f6680be4ef9c4e7119380152bd70bff314
unknown <jharleymark98@gmail.com>
2024-05-03 14:52:55 +0800

    3.5.24

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,15 @@
 import type { Metadata } from "next";
-import { DM_Sans } from "next/font/google";
+import { DM_Sans, Roboto } from "next/font/google";
 import "./globals.css";
 import { LayoutWrapper } from "@/components/layout/LayoutWrapper";
 import NavigationBar from "@/components/navigation";
 import NavigationFooter from "@/components/navigation/footer";
 import MobileNav from "@/components/navigation/mobile-nav";
 // const inter = Inter({ subsets: ["latin"] });
-const font = DM_Sans({subsets:['latin']})
+const font = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
   title: "AI SaaS",
